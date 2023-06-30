a42883a7708e6f697c97eb5c374d786101f1e824
Kumar Subham Krishna <65730656+ksubhamkrishna@users.noreply.github.com>
2023-06-30 17:25:50 +0530

    Add files via upload

---
+++ MycoolappApplication.java
@@ -0,0 +1,15 @@
+package com.luv2code.springboot.demp.mycoolapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MycoolappApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(MycoolappApplication.class, args);
+
+	}
+
+}
